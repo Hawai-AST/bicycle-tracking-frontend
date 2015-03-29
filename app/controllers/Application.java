46769bf137c05cd39cd595a79a72acf2c89fd822
Timmay24 <tim.hartig@haw-hamburg.de>
2015-03-29 17:37:09 +0200

    Modified views (index, main) and added actions to Application controller as well as routes for upcoming signin pages

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,34 +1,51 @@
 package controllers;
 
 
-import models.Bar;
-import org.springframework.beans.factory.annotation.Autowired;
+import models.Login;
+import models.Registration;
 import org.springframework.stereotype.Controller;
 import play.data.Form;
 import play.libs.Json;
 import play.mvc.Result;
-import services.BarService;
 import views.html.index;
+import views.html.signin;
+
+import static play.mvc.Results.ok;
 
 @Controller
 public class Application {
 
-    @Autowired
-    private BarService barService;
-
     public Result index() {
-        return play.mvc.Controller.ok(index.render(Form.form(Bar.class)));
+        return ok(index.render());
     }
 
-    public Result addBar() {
-        Form<Bar> form = Form.form(Bar.class).bindFromRequest();
-        Bar bar = form.get();
-        barService.addBar(bar);
-        return play.mvc.Controller.redirect(controllers.routes.Application.index());
+    public Result signin() {
+        return ok(signin.render(
+                "Anmelden",
+                Form.form(Login.class),
+                Form.form(Registration.class)
+        ));
     }
 
-    public Result listBars() {
-        return play.mvc.Controller.ok(Json.toJson(barService.getAllBars()));
+    public Result register() {
+        Form<Registration> form = Form.form(Registration.class).bindFromRequest();
+        Registration registration = form.get();
+        return ok("Hallo " + registration.firstName + "! \n" + "Du hast Dich erfolgreich registriert." +
+                "\n" + Json.toJson(registration));
+
+        // TODO Exception Handling bzgl. validate()
+
+        // TODO Send JSON authentication to backend
     }
-    
+
+    public Result authenticate() {
+        Form<Login> form = Form.form(Login.class).bindFromRequest();
+        Login login = form.get();
+        return ok("You entered the following email: " + login.email + "\n" + Json.toJson(login));
+
+        // TODO Exception Handling bzgl. validate()
+
+        // TODO Send JSON registration to backend
+    }
+
 }
