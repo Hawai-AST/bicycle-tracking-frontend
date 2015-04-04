50c496bd2b5115c747af02f8eb5eb760e5506743
Timmay24 <tim.hartig@haw-hamburg.de>
2015-04-04 12:53:08 +0200

    Removed dummy code. Renderings temporarily return JSON objects from signin forms.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,13 @@
 package controllers;
 
+// TODO REST implementation --> communicate with backend
+// TODO store sessions
+
 
 import models.Login;
 import models.Registration;
 import org.springframework.stereotype.Controller;
 import play.data.Form;
-import play.libs.Json;
 import play.mvc.Result;
 import views.html.index;
 import views.html.signin;
@@ -15,9 +17,7 @@ import static play.mvc.Results.ok;
 @Controller
 public class Application {
 
-    public Result index() {
-        return ok(index.render());
-    }
+    public Result index() { return ok(index.render()); }
 
     public Result signin() {
         return ok(signin.render(
@@ -30,22 +30,15 @@ public class Application {
     public Result register() {
         Form<Registration> form = Form.form(Registration.class).bindFromRequest();
         Registration registration = form.get();
-        return ok("Hallo " + registration.firstName + "! \n" + "Du hast Dich erfolgreich registriert." +
-                "\n" + Json.toJson(registration));
 
-        // TODO Exception Handling bzgl. validate()
-
-        // TODO Send JSON authentication to backend
+        return ok(registration.toJson());
     }
 
     public Result authenticate() {
         Form<Login> form = Form.form(Login.class).bindFromRequest();
         Login login = form.get();
-        return ok("You entered the following email: " + login.email + "\n" + Json.toJson(login));
 
-        // TODO Exception Handling bzgl. validate()
-
-        // TODO Send JSON registration to backend
+        return ok(login.toJson());
     }
 
 }
