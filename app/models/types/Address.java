0df77c6e1bd748ecb95d75a63067a71273e7f140
Timmay24 <tim.hartig@haw-hamburg.de>
2015-04-04 11:52:06 +0200

    Added Address data type reflecting the backend data type (but added following attributes: String streetNo, String country)

---
+++ app/models/types/Address.java
@@ -0,0 +1,10 @@
+package models.types;
+
+public class Address {
+    private String street;
+    private String streetNo;
+    private String city;
+    private String postcode;
+    private String country;
+    private String state;
+}
