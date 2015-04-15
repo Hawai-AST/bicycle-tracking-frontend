4a121223f9f24f37507b819fae6122995dc1ea3d
Timmay24 <tim.hartig@haw-hamburg.de>
2015-04-15 18:54:55 +0200

    Add javadoc

--- app/controllers/util/ASTPreparedJson.java
+++ app/controllers/util/ASTPreparedJson.java
@@ -17,20 +17,54 @@ public class ASTPreparedJson {
                 .setContentType("application/json");
     }
 
+    /**
+     * Sets the url for the request
+     *
+     * @param url The url to invoke later
+     * @return Modified wsRequestHolder
+     */
     public WSRequestHolder url(String url) {
         wsRequestHolder = WS.url(url);
         return wsRequestHolder;
     }
 
+    /**
+     * Sets the content type for the request
+     *
+     * @param name Name of the key which shall be added
+     * @param value Value of the key
+     * @return Modified wsRequestHolder
+     */
     public WSRequestHolder setHeader(String name, String value) {
         return wsRequestHolder.setHeader(name, value);
     }
 
+    /**
+     * Sets the content type for the request
+     *
+     * @param contentType Content type of the request
+     * @return Modified wsRequestHolder
+     */
     public WSRequestHolder setContentType(String contentType) {
         return wsRequestHolder.setContentType(contentType);
     }
 
-    public F.Promise<JsonNode> post(com.fasterxml.jackson.databind.JsonNode body) {
+    /**
+     * Posts the given json body to the specified url
+     *
+     * @param body Body of JSON node to post
+     * @return Promised response from post call
+     */
+    public F.Promise<JsonNode> post(JsonNode body) {
         return wsRequestHolder.post(body).map(WSResponse::asJson);
+    }
+
+    /**
+     * Calls GET method on the request
+     *
+     * @return Promised response from get call
+     */
+    public F.Promise<WSResponse> get() {
+        return wsRequestHolder.get();
     }
 }
