fd27c2722aed26a51e87b833a2e1d6aad1c45844
Timmay24 <tim.hartig@haw-hamburg.de>
2015-04-23 11:00:32 +0200

    Add stub of a Authentication controller

---
+++ app/controllers/Authentication.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import play.mvc.Result;
+import play.mvc.Results;
+
+@Controller
+public class Authentication {
+
+    public static Result register() {
+        return Results.TODO;
+    }
+
+}
