cf8414e6f9975489dc3fc608366827306333cf6f
TorbenHaug <claustorbenhaug@hotmail.de>
2015-05-05 22:02:29 +0200

    ASt89 - Add new url

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -73,7 +73,7 @@ public class Application {
         //JsonNode jsonResponse = doRequest("http://localhost:8080/api/v1/login", login.toJson());
 
         int responseTimeoutInMs = 10000;
-        PreparedJson preparedJson = AST.preparedJson("http://localhost:8080/api/v1/login");
+        PreparedJson preparedJson = AST.preparedJson("http://localhost:8080/api/oauth/token");
         preparedJson.setContentType("application/x-www-form-urlencoded");
         F.Promise<JsonNode> jsonPromise = preparedJson.post(login.toJson());
 
