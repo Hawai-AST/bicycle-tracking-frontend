fbfdbd9a5696fbe28ef8e1ea949e44b22cb91859
Timmay24 <tim.hartig@haw-hamburg.de>
2015-05-06 11:24:37 +0200

    Fix #AST-64 Fix typo,  comply with check style

--- app/controllers/Account.java
+++ app/controllers/Account.java
@@ -1,27 +1,22 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import models.ChangePassword;
 import models.ChangeUserCredentials;
 import models.utility.AST;
-import play.libs.ws.WSResponse;
 import play.data.Form;
 import play.data.validation.ValidationError;
 import play.libs.F;
+import play.libs.ws.WSResponse;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.member.account;
 
-import java.io.IOException;
 import java.util.List;
 
 import static play.data.Form.form;
 
-/**
- * Created by Admin on 03.05.2015.
- */
-public class Account extends Controller{
+public class Account extends Controller {
     final static Form<ChangePassword> changePasswordForm = form(ChangePassword.class);
     final static Form<ChangeUserCredentials> changeUserCredentialsForm = form(ChangeUserCredentials.class);
 
@@ -29,31 +24,31 @@ public class Account extends Controller{
         F.Promise<JsonNode> response = AST.preparedJson("http://localhost:8080/api/v1/user").get().map(WSResponse::asJson);
         JsonNode node = response.get(10000);
         ChangeUserCredentials changeUserCredentials = ChangeUserCredentials.fromJson(node);
-        Form<ChangeUserCredentials> preFilledUserCredentialsForm = changeUserCredentialsForm.fill(changeUserCredentials);
+        Form<ChangeUserCredentials> prefilledUserCredentialsForm = changeUserCredentialsForm.fill(changeUserCredentials);
 
-        return ok(account.render(changePasswordForm, preFilledUserCredentialsForm));
+        return ok(account.render(changePasswordForm, prefilledUserCredentialsForm));
     }
 
-    public static Result changePassword(){
+    public static Result changePassword() {
         Form<ChangePassword> filledForm = changePasswordForm.bindFromRequest();
-        if(filledForm.hasErrors()){
-            for(String key : filledForm.errors().keySet()){
+        if (filledForm.hasErrors()) {
+            for (String key : filledForm.errors().keySet()) {
                 List<ValidationError> currentError = filledForm.errors().get(key);
-                for(ValidationError error : currentError){
+                for (ValidationError error : currentError) {
                     flash("alert", error.message());
                 }
             }
             flash("alert_type", "danger");
             return badRequest(account.render(filledForm, changeUserCredentialsForm));
-        }else {
+        } else {
             ChangePassword changePassword = filledForm.get();
             JsonNode jsonResponse = Application.doRequest("http://localhost:8080/api/v1/user/password", changePassword.toJson());
-            if(jsonResponse != null){
+            if (jsonResponse != null) {
                 flash("alert", "Passwort konnte nicht geändert werden");
                 flash("alert_type", "danger");
                 return ok(jsonResponse.asText());
-            }else {
-                flash("alert", "Passwort wurde erfolgreich geaendert");
+            } else {
+                flash("alert", "Passwort wurde erfolgreich geändert");
                 flash("alert_type", "success");
                 return ok(account.render(filledForm, changeUserCredentialsForm));
             }
@@ -61,18 +56,18 @@ public class Account extends Controller{
         }
     }
 
-    public static Result changeUserCredentials(){
+    public static Result changeUserCredentials() {
         Form<ChangeUserCredentials> filledForm = changeUserCredentialsForm.bindFromRequest();
-        if(filledForm.hasErrors()){
+        if (filledForm.hasErrors()) {
             flash("alert", "Daten konnten nicht gespeichert werden");
             flash("alert_type", "danger");
             return badRequest(account.render(changePasswordForm, filledForm));
-        }else {
-            if(Application.doRequest("http://localhost:8080/api/v1/user", filledForm.get().toJson())!=null){
+        } else {
+            if (Application.doRequest("http://localhost:8080/api/v1/user", filledForm.get().toJson()) != null) {
                 flash("alert", "Daten konnten nicht gespeichert werden");
                 flash("alert_type", "danger");
                 return badRequest(Application.doRequest("http://localhost:8080/api/v1/user", filledForm.get().toJson()));//account.render(changePasswordForm, filledForm));
-            }else {
+            } else {
                 flash("alert", "Daten wurden geändert");
                 flash("alert_type", "success");
                 return ok(account.render(changePasswordForm, filledForm));
