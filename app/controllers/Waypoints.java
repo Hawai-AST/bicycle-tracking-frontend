ba52d9fa32fefd52dc4ef6b537f3e89814407bc6
Timmay24 <tim.hartig@haw-hamburg.de>
2015-05-06 14:50:20 +0200

    Add waypoints controller

---
+++ app/controllers/Waypoints.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Waypoints extends Controller {
+
+    public static Result create() {
+        return ok("'test'");
+    }
+}
