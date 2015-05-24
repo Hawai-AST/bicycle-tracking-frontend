09170ca45a8a8081c21e465cfb4e5f64755d31c5
Marjan Bachtiari <marjan.bachtiari@haw-hamburg.de>
2015-05-25 01:38:44 +0200

    Try fix create in maps controller, JSON object correct but route still wrong

--- app/controllers/Maps.java
+++ app/controllers/Maps.java
@@ -1,7 +1,14 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import models.TrackRegistration;
+import models.utility.AST;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.helper.form;
+
+import java.util.HashMap;
 
 public class Maps extends Controller {
     // TODO (Marjan) impl correct return
@@ -10,6 +17,22 @@ public class Maps extends Controller {
      * Creates and saves route
      * @return
      */
+//    public static Result create(JsonNode newRoute) {
+//        // TODO (Marjan) post to BE properly
+//        // TODO statt form.get() ajax json rein
+//        Form<TrackRegistration> form = Form.form(TrackRegistration.class).bindFromRequest();
+//        String currentUserAddress = AST.getUserAddress();
+//
+//        if (Application.doRequest("http://localhost:8080/api/v1/route", newRoute) != null) {
+//            flash("alert", "Strecke konnte nicht gespeichert werden");
+//            flash("alert_type", "danger");
+//            return badRequest(Application.doRequest("http://localhost:8080/api/v1/route", newRoute));
+//        } else {
+//            flash("alert", "Strecke wurde gespeichert");
+//            flash("alert_type", "success");
+//            return ok(views.html.member.newtrack.render(form, currentUserAddress));
+//        }
+//    }
     public static Result create() {
         return ok("'test'");
     }
