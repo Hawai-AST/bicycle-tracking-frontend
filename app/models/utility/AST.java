fda9624be8b3ac67569ec0a502de9dd57490ee7e
Tim Hagemann <tim@infinityblade.de>
2015-06-02 17:33:12 +0200

    Removed things that got accidentally added from deploy

--- app/models/utility/AST.java
+++ app/models/utility/AST.java
@@ -4,11 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import com.fasterxml.jackson.databind.JsonNode;
 import config.BackendConfig;
-import models.Bike;
-import models.Track;
 import play.libs.ws.WSResponse;
-
-import java.util.*;
 
 /**
  * Factory Class
@@ -32,49 +28,6 @@ public class AST {
             inverseMap.put(entry.getValue(), entry.getKey());
         }
         return inverseMap;
-    }
-
-    /**
-     *
-     * @return bikes name of logged in user
-     */
-    public static List<Bike> bikeMap() {
-        JsonNode response = AST.preparedJson(BackendConfig.backendURL() + "/api/v1/bikes").get().map(WSResponse::asJson).get(10000);
-        List<Bike> bikeList = new ArrayList<Bike>();
-        for (JsonNode bikeNode : response.get("bikes")) {
-            bikeList.add(Bike.fromJson(bikeNode));
-        }
-        return bikeList;
-    }
-
-    /**
-     * Gets the saved tracks
-     * @return a list of the object Track. If no route was saved before
-     * return list is empty.
-     */
-    public static List<Track> trackNameMap() {
-        List<Track> trackList = new ArrayList<>();
-        JsonNode response = AST.preparedJson(BackendConfig.backendURL() + "/api/v1/route").get().map(WSResponse::asJson).get(10000);
-        System.err.println("-------AST Z56 : this is the track response: " + response.toString());
-
-        for (int i = 0; i < response.size(); i++){
-            trackList.add(Track.fromJson(response.get(i)));
-        }
-
-        return trackList;
-    }
-
-    /**
-     * Gets the track by id.
-     * @param id is a UUID as a String.
-     * @return a object of Track
-     */
-    public static JsonNode getTrack(String id){
-        // TODO(Louisa / Marjan) use/get correct id
-        JsonNode response = AST.preparedJson(BackendConfig.backendURL() + "/api/v1/route/" + id).get().map(WSResponse::asJson).get(10000);
-        System.out.println("-------AST Z73 : this is the route" + response);
-        Track retTrack = Track.fromJson(response);
-        return response;
     }
 
     public static String getUserAddress(){
